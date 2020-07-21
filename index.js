dbe31b69223ca19cc40060b1a705b510e783ca66
Mariana Mazzariello <mfmazzariello@gmail.com>
2020-07-21 12:08:21 +0200

    Finally version lab

--- index.js
+++ index.js
@@ -1,17 +1,50 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.length++;
+    this.items.sort((a, b) => a - b);
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos <= this.length) {
+      var number = this.items[pos];
+      return number;
+    } else {
+      throw new Error("OutOfBounds");
+    }
+  }
 
-  max() {}
+  max() {
+    if (this.length > 0) {
+      return Math.max(...this.items);
+    } else {
+      throw new Error("EmptySortedList");
+    }
+  }
 
-  min() {}
+  min() {
+    if (this.length > 0) {
+      return Math.min(...this.items);
+    } else {
+      throw new Error("EmptySortedList");
+    }
+  }
 
-  sum() {}
+  sum() {
+    if (this.length > 0) {
+      return this.items.reduce((a, b) => a + b);
+    } else return 0;
+  }
 
-  avg() {}
+  avg() {
+    var total= this.items.reduce((a, b) => a + b);
+    return total/this.length
+  } 
 }
 
 module.exports = SortedList;
--- test/index.spec.js
+++ test/index.spec.js
@@ -155,3 +155,5 @@ describe("SortedList", () => {
     });
   });
 });
+
+
